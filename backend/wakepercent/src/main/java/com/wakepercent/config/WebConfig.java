6763914cbe0a1443284d77c143fb25d9d65f81a7
wakebro <wakebro119@naver.com>
2024-11-10 22:22:28 -0500

    - Add new static directory and path for file

--- backend/wakepercent/src/main/java/com/wakepercent/config/WebConfig.java
+++ backend/wakepercent/src/main/java/com/wakepercent/config/WebConfig.java
@@ -3,11 +3,18 @@ package com.wakepercent.config;
 import com.wakepercent.commonEntity.ContentTypeConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new ContentTypeConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static_backend/**")
+                .addResourceLocations("classpath:/static_backend/");
     }
 }
